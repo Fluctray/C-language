d5d486c02a1b66afa02f5a72e8f266cb5af745e6
Fluctray <46572188+Fluctray@users.noreply.github.com>
2019-03-20 16:06:40 -0600

    Create unfixed_reserve.c

---
+++ unfixed_reserve.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define N 10
+int main(){
+    int a[i], *p;
+    printf("Enter arrys:");
+    for(p = &a[0]; p<&a[i]; p++){
+        scanf("%d",&a[i]);
+    }
+    for(p = &a[0]; p<&a[i]; p++){
+    //for(p = &a[N]; p>&a[0]; p--){
+        printf("%d", a[i]);
+    }
+}
